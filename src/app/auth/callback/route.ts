527c29c8e21162943fd8374799c83dd2211e9d45
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-03-18 13:46:09 +0600

    fix: redirect url

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -18,5 +18,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign up process completes
-  return NextResponse.redirect(`${origin}/create-story`);
+  return NextResponse.redirect(`${process.env.REDIRECT_URL}/create-story`);
 }
